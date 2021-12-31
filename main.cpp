4c7eb6f6fb3c2de2e3a3c5fd16ac6b54c72714ea
mcmarius <m.c.marius@outlook.com>
2021-12-31 22:37:07 +0200

    Ignore unused variables

--- main.cpp
+++ main.cpp
@@ -1,7 +1,7 @@
 #define SDL_MAIN_HANDLED 1
 #include <SDL.h>
 
-int main(int argc, char *argv[])
+int main([[maybe_unused]] int argc, [[maybe_unused]] char *argv[])
 {
     SDL_Window *window = SDL_CreateWindow("sound-synth", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, 600, 600, SDL_WINDOW_OPENGL);
     SDL_DestroyWindow(window);
