48d766bb077e82f361b3672fc26c61cd68c4c13e
mcmarius <m.c.marius@outlook.com>
2022-03-11 01:56:34 +0200

    Try to add more input keys

--- run.sh
+++ run.sh
@@ -1,4 +1,7 @@
 # sleep 2 && xdotool search --name "My Window" key X &
-DISPLAY=:99.0 ASAN_OPTIONS=halt_on_error=0 sleep 1 && xdotool search --name "My Window" key X &
+sleep 1 && xdotool search --name "My Window" key X &
+sleep 1 && xdotool search --name "My Window" key Y &
+sleep 1 && xdotool search --name "My Window" key X &
+sleep 1 && xdotool search --name "My Window" key Z &
 DISPLAY=:99.0 ASAN_OPTIONS=halt_on_error=0 ./artifacts/bin/demo-sfml
 # wmctrl -lx
