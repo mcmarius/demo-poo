d0a591b8a2bb75922989fd3d1ef36489779d95a1
mcmarius <m.c.marius@outlook.com>
2022-03-11 00:02:57 +0200

    Try to send events again

--- run.sh
+++ run.sh
@@ -1,4 +1,4 @@
 
-# sleep 1 && xdotool search --name "My Window" windowactivate --sync %1 key X &
-./artifacts/bin/demo-sfml &
-wmctrl -lx
+sleep 2 && xdotool search --name "My Window" key X &
+./artifacts/bin/demo-sfml
+# wmctrl -lx
