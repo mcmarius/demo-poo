7f8ded86bb2662473677f0dddf7bbae9f1c680be
mcmarius <m.c.marius@outlook.com>
2022-03-11 01:49:40 +0200

    Install more tools, do not run exe in bg

--- run.sh
+++ run.sh
@@ -1,8 +1,4 @@
 # sleep 2 && xdotool search --name "My Window" key X &
-env
-DISPLAY=:99.0
-ASAN_OPTIONS=halt_on_error=0
-env
 DISPLAY=:99.0 ASAN_OPTIONS=halt_on_error=0 sleep 1 && xdotool search --name "My Window" key X &
-DISPLAY=:99.0 ASAN_OPTIONS=halt_on_error=0 ./artifacts/bin/demo-sfml &
+DISPLAY=:99.0 ASAN_OPTIONS=halt_on_error=0 ./artifacts/bin/demo-sfml
 # wmctrl -lx
