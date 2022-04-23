ce63b1ba48b23f039cf2be10f8b75816d75d3d4a
mcmarius <m.c.marius@outlook.com>
2022-04-24 02:32:26 +0300

    Sleep more

--- scripts/run.sh
+++ scripts/run.sh
@@ -3,6 +3,6 @@ sleep 1 && xdotool search --name "My Window" key X &
 sleep 1 && xdotool search --name "My Window" key Y &
 sleep 1 && xdotool search --name "My Window" key X &
 sleep 1 && xdotool search --name "My Window" key Z &
-sleep 3 && xdotool search --name "My Window" key Alt+F4 &
+sleep 13 && xdotool search --name "My Window" key Alt+F4 &
 DISPLAY=:99.0 ASAN_OPTIONS=halt_on_error=0 ./artifacts/bin/demo-sfml
 # wmctrl -lx
--- scripts/run_valgrind.sh
+++ scripts/run_valgrind.sh
@@ -2,6 +2,6 @@ sleep 2 && xdotool search --name "My Window" key X &
 sleep 2 && xdotool search --name "My Window" key Y &
 sleep 2 && xdotool search --name "My Window" key X &
 sleep 2 && xdotool search --name "My Window" key Z &
-sleep 4 && xdotool search --name "My Window" key Alt+F4 &
+sleep 24 && xdotool search --name "My Window" key Alt+F4 &
 valgrind --leak-check=full --error-exitcode=0 ./artifacts/bin/demo-sfml
 
