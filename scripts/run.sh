1a477ef426199cd698feca3e7ac7c82a9639a2da
mcmarius <mcmarius@users.noreply.github.com>
2022-04-24 02:57:48 +0300

    Try to fix scripts

--- scripts/run.sh
+++ scripts/run.sh
@@ -1,8 +1,7 @@
-# sleep 2 && xdotool search --name "My Window" key X &
-sleep 1 && xdotool search --name "My Window" key X &
-sleep 1 && xdotool search --name "My Window" key Y &
-sleep 1 && xdotool search --name "My Window" key X &
-sleep 1 && xdotool search --name "My Window" key Z &
-sleep 13 && xdotool search --name "My Window" key Alt+F4 &
+sleep 3 && xdotool search --name "My Window" key X &&
+sleep 1 && xdotool search --name "My Window" key Y &&
+sleep 1 && xdotool search --name "My Window" key X &&
+sleep 1 && xdotool search --name "My Window" key Z &&
+sleep 2 && xdotool search --name "My Window" key Alt+F4 &
 DISPLAY=:99.0 ASAN_OPTIONS=halt_on_error=0 ./artifacts/bin/demo-sfml
 # wmctrl -lx
--- scripts/run_valgrind.sh
+++ scripts/run_valgrind.sh
@@ -1,7 +1,7 @@
-sleep 2 && xdotool search --name "My Window" key X &
-sleep 2 && xdotool search --name "My Window" key Y &
-sleep 2 && xdotool search --name "My Window" key X &
-sleep 2 && xdotool search --name "My Window" key Z &
-sleep 24 && xdotool search --name "My Window" key Alt+F4 &
+sleep 10 && xdotool search --name "My Window" key X &&
+sleep 1 && xdotool search --name "My Window" key Y &&
+sleep 1 && xdotool search --name "My Window" key X &&
+sleep 1 && xdotool search --name "My Window" key Z &&
+sleep 2 && xdotool search --name "My Window" key Alt+F4 &
 valgrind --leak-check=full --error-exitcode=0 ./artifacts/bin/demo-sfml
 
