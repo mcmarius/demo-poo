93f47ab8355838b1fa0f0e9ee777fc6c0e00f4ef
mcmarius <m.c.marius@outlook.com>
2022-04-24 03:40:49 +0300

    Less checks in valgrind due to errs in ext deps

--- scripts/run_valgrind.sh
+++ scripts/run_valgrind.sh
@@ -1,4 +1,4 @@
-valgrind --leak-check=full --error-exitcode=0 ./artifacts/bin/demo-sfml &
+valgrind --error-exitcode=0 ./artifacts/bin/demo-sfml &
 sleep 10 && xdotool search --name "My Window" key X &&
 sleep 1 && xdotool search --name "My Window" key Y &&
 sleep 1 && xdotool search --name "My Window" key X &&
