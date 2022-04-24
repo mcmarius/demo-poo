eaf4fc642f5fe98a2ec219855352b703093b807b
mcmarius <mcmarius@users.noreply.github.com>
2022-04-24 03:30:48 +0300

    Try again

--- scripts/run.sh
+++ scripts/run.sh
@@ -1,7 +1,9 @@
+./artifacts/bin/demo-sfml &
 sleep 3 && xdotool search --name "My Window" key X &&
 sleep 1 && xdotool search --name "My Window" key Y &&
 sleep 1 && xdotool search --name "My Window" key X &&
 sleep 1 && xdotool search --name "My Window" key Z &&
-sleep 2 && xdotool search --name "My Window" key Alt+F4 &
-DISPLAY=:99.0 ASAN_OPTIONS=halt_on_error=0 ./artifacts/bin/demo-sfml
+sleep 2 && xdotool search --name "My Window" key Alt+F4
+#DISPLAY=:99.0 ASAN_OPTIONS=halt_on_error=0 ./artifacts/bin/demo-sfml
+#ASAN_OPTIONS=halt_on_error=0 ./artifacts/bin/demo-sfml
 # wmctrl -lx
