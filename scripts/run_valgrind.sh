8008ea799fa09b95901f6b71983e375ed472686e
mcmarius <mcmarius@users.noreply.github.com>
2022-04-24 03:34:05 +0300

    Also try with valgrind

--- scripts/run_valgrind.sh
+++ scripts/run_valgrind.sh
@@ -1,7 +1,7 @@
+valgrind --leak-check=full --error-exitcode=0 ./artifacts/bin/demo-sfml &
 sleep 10 && xdotool search --name "My Window" key X &&
 sleep 1 && xdotool search --name "My Window" key Y &&
 sleep 1 && xdotool search --name "My Window" key X &&
 sleep 1 && xdotool search --name "My Window" key Z &&
-sleep 2 && xdotool search --name "My Window" key Alt+F4 &
-valgrind --leak-check=full --error-exitcode=0 ./artifacts/bin/demo-sfml
+sleep 2 && xdotool search --name "My Window" key Alt+F4
 
