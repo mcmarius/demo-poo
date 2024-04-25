22751cc3f0215515dfb42d9aa903655565979d2f
mcmarius <23401453+mcmarius@users.noreply.github.com>
2024-04-26 02:14:12 +0300

    Test example, fix macOS

--- main.cpp
+++ main.cpp
@@ -4,9 +4,6 @@
 
 #include <iostream>
 #include <concepts>
-//#define OLC_PGE_APPLICATION
-
-#include <olcPixelGameEngine.h>
 
 template <typename T>
 concept Printable = requires(const T& v, std::ostream& os) {
@@ -41,7 +38,38 @@ public:
     }
 };
 
+// Override base class with your custom functionality
+class Example : public olc::PixelGameEngine
+{
+public:
+	Example()
+	{
+		// Name your application
+		sAppName = "Example";
+	}
+
+public:
+	bool OnUserCreate() override
+	{
+		// Called once at the start, so create things here
+		return true;
+	}
+
+	bool OnUserUpdate(float fElapsedTime) override
+	{
+		// Called once per frame, draws random coloured pixels
+		for (int x = 0; x < ScreenWidth(); x++)
+			for (int y = 0; y < ScreenHeight(); y++)
+				Draw(x, y, olc::Pixel(rand() % 256, rand() % 256, rand() % 256));
+		return true;
+	}
+};
+
 int main() {
+	Example demo;
+	if (demo.Construct(256, 240, 4, 4))
+		demo.Start();
+
     def<instrument> d;
     d.f();
 
